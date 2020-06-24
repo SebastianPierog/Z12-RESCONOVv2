7541c8c9e7cda0bfdf968bf3f26ed81548d13844
Pan Ktos <lambda19935@gmail.com>
2020-06-24 14:39:57 +0200

    Dodaj pliki projektów.

---
+++ Z15-RESCONOV/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Z15_RESCONOV
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        //public Model Model { get; set; } = 100;
+        public Model Model { get; set; } = new Model();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = Model;
+        }
+
+
+        private void Grid_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            var test = this.FindResource("Title");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["Brush"] = new SolidColorBrush(Colors.CornflowerBlue);
+            var res = new ResourceDictionary();
+            res.Add("test", "test");
+            Resources.MergedDictionaries.Add(res);
+        }
+
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Model.Zoom += 10;
+        }
+    }
+}
+
---
+++ Z15-RESCONOV/Model.cs
@@ -0,0 +1,37 @@
+﻿using Hangfire.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z15_RESCONOV
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private int _zoom;
+
+        public int Zoom
+        {
+            get => _zoom;
+            set
+            {
+                if (_zoom != value)
+                {
+                    _zoom = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
